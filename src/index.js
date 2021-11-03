09eaeaf1e38c9af244c2e0aa0aefe839865ea0d7
Sourav <souravdas.oo10@gmail.com>
2021-11-03 19:37:51 +0530

    update code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const config = require("./config/defaultConfig");
 app.set("port", process.env.PORT);
 const server = http.createServer(app);
 // Start a TCP server listening for connections on the given port and host
-server.listen(config[config.env].port, () => {
-  console.log(`Server running at ${config[config.env].port}`);
+server.listen(process.env.PORT||5000, () => {
+  console.log(`Server running at ${process.env.PORT}`);
 });
